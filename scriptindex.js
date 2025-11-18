87a1ac9ddd899d1c2e764a4daeae432a0dccc0b3
eckin33 <erickdubisoj4@gmail.com>
2025-11-17 22:32:39 -0300

    form fix

--- scriptindex.js
+++ scriptindex.js
@@ -16,7 +16,7 @@ let enter = document.getElementById('ipass').addEventListener('keydown', (e) => 
 })
  */
 
-document.getElementById("go").addEventListener("submit", async (e) => {
+document.getElementById("formCadastro").addEventListener("submit", async (e) => {
         e.preventDefault();
 
         const formData = {
